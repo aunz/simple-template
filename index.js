eac2626aa0b4cffc076065633f0c84aff8aa144c
aunz <aunz10@yahoo.com>
2015-12-26 02:02:05 +1300

    add test

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Replace a string with multiple matches
+ * @arg {string} string 
+ * @arg {object} replacement
+ * @return {string}
+ * @example 
+ * template("My name is {{name}}, {{age}} years old.",{name:'kaka', age:11})
+ * // return "My name is kaka, 11 years old."
+ */
+
+export default (string,replacement) => {
+	const map = Object.keys(replacement).reduce((p,n) => {		
+		return {...p,['{{'+n+'}}']:replacement[n]}
+	},{})	
+	
+	const re = new RegExp(Object.keys(map).join('|'),'g')
+	return string.replace(re,(match)=>{		
+		return map[match]
+	})
+} 
---
+++ test/index.js
@@ -0,0 +1,30 @@
+import test from 'tape'
+
+import template from '../index.js'
+
+// var test = require('tape')
+
+
+test('should throw', function (t) {	
+  t.throws(()=>{template()})
+  t.throws(()=>{template(null,null)})
+  t.throws(()=>{template(null,{})})
+  t.end()
+})
+
+test('string replacement', function (t) {	
+	let actual = template(`My name is {{name}}, {{age}} years old, from {{place}}.`, {name:'kaka', age: 11, place: 'earth'})
+  t.equal(actual,'My name is kaka, 11 years old, from earth.')
+  
+  actual = template(`{{n}}la{{n}}la{{n}}`,{n:3,m:2})
+  t.equal(actual,`3la3la3`)
+
+  actual = template(`{{n}}la{{n}}la{{n}}`,{n:3,n:4})
+  t.equal(actual,`4la4la4`)
+
+	actual = template(`{{ m }}`,{m:2})
+  t.equal(actual,`{{ m }}`)
+  
+
+  t.end()
+})
